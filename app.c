af64dae560c2077a431f04b3149f94dac7ea4280
AbdeLilah <you@example.com>
2024-06-02 22:13:26 +0200

    testing my code

---
+++ app.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int age, num;
+	printf("enter your number in class : ");
+	scanf("%d", &num);
+
+	printf("enter your age : ");
+	scanf("%d", &age);
+	
+	printf("your age is : %d, and your num is : %d\n", age, num);
+
+	return 0;
+}
